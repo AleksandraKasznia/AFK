aca82c5ab179f7dbe1afc878db82094fb653e493
Shinigami072 <krzys.stasiowski@gmail.com>
2019-03-22 23:00:42 +0100

    Test

--- src/main/java/AFK/demo/Skill.java
+++ src/main/java/AFK/demo/Skill.java
@@ -1,17 +1,15 @@
-//package AFK.demo;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Table(name = "skill", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
-//
-//public class Skill {
-//    @Id
-//    private Long id;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user.username")
-//    private User user;
-//
-//
-//}
+package AFK.demo;
+
+import javax.persistence.*;
+
+@Entity
+public class Skill {
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user.username")
+    private User user;
+
+
+}

c6ff2fc6c56b986b3ae9edd1ab7636e6fa93c5a0
AleksandraKasznia <33521222+AleksandraKasznia@users.noreply.github.com>
2019-03-23 02:55:32 +0100

    Added adding user

--- src/main/java/AFK/demo/UserController.java
+++ src/main/java/AFK/demo/UserController.java
@@ -3,8 +3,12 @@ package AFK.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,4 +36,13 @@ public class UserController {
         model.addAttribute("skills",":"+skills.toString());
         return "user";
     }
+
+    @PostMapping("/user/add")
+    public String addUser(@ModelAttribute("userForm")User user,
+                          BindingResult result, Model model,
+                          final RedirectAttributes redirectAttributes){
+        userRepository.save(user);
+        return "/";
+    }
+
 }

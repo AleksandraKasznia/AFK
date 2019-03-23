2f44948023b0281adb9eb9825b0ac0070f5ae65c
AleksandraKasznia <33521222+AleksandraKasznia@users.noreply.github.com>
2019-03-23 04:43:07 +0100

    Added candidate table

--- src/main/java/AFK/demo/Candidate.java
+++ src/main/java/AFK/demo/Candidate.java
@@ -1,7 +1,15 @@
 package AFK.demo;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
 @Entity
 public class Candidate {
+    @EmbeddedId
+    private CandidateKey candidateKey;
+
+    @Override
+    public String toString() {
+        return candidateKey.getUsername();
+    }
 }
--- src/main/java/AFK/demo/CandidateKey.java
+++ src/main/java/AFK/demo/CandidateKey.java
@@ -1,4 +1,52 @@
 package AFK.demo;
 
-public class CandidateKey {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class CandidateKey implements Serializable{
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "project")
+    private String id;
+
+    public CandidateKey() { }
+
+    public CandidateKey(String username, String projectId) {
+        this.username = username;
+        this.id = projectId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getProjectId() {
+        return id;
+    }
+
+    public void setProjectId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CandidateKey)) return false;
+        CandidateKey that = (CandidateKey) o;
+        return Objects.equals(getUsername(), that.getUsername()) &&
+                Objects.equals(getProjectId(), that.getProjectId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUsername(), getProjectId());
+    }
 }
